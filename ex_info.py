e6c39421a4c0a87596c8b949cb70539881abec92
rudneva.iv@phystech.edu <rudneva.iv@phystech.edu>
2023-04-18 23:54:02 +0300

    change

---
+++ ex_info.py
@@ -0,0 +1,15 @@
+ex_code_dct = {
+    101: "incorrect password",
+    102: "user not found",
+    103: "login is already existed",
+    111: "unavailable account number",
+    112: "amount should be upper than 0",
+    113: "unavailable account type",
+    301: "insufficient funds to withdraw",
+    302: "insufficient funds for the transfer",
+    311: "validity_period is not finished",
+    312: "withdrawal limit exceeded",
+    412: "limited account. transfer limit exceeded",
+    413: "limited account. withdraw limit exceeded",
+    404: "non-existent account id"
+    }
